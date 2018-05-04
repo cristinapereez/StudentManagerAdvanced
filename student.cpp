6087462c474c931ca5089dbba538459e2f309700
cristinapereez <cperez58@mail.csuchico.edu>
2018-05-03 22:52:08 -0700

    try again

--- student.cpp
+++ student.cpp
@@ -32,7 +32,7 @@ student::student()
         list.pop_back();
 
         //  double average = 0;
-          double studentGrade = 0;
+//          double studentGrade = 0;
             double sum = 0;
               for(int i =0; i < list.size(); i++)
                 {
